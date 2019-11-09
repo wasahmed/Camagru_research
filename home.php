912974eeffa39981563337e9219a9d3742c2ccbe
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-11-09 16:16:12 +0200

    fix upload

--- home.php
+++ home.php
@@ -41,6 +41,29 @@
                     else if ($link == '3')
                     {
                         echo "post image";
+                        echo "<form action='' method='post' enctype='multipart/form-data'>";
+                        echo "<input type='file' name='image'/>
+                        <input type='submit' name='insert_post' value='Post'/>";
+                        // if(isset($_POST['insert_post']))
+                        // {
+                        // $product_title = $_POST['product_title'];
+                        // $product_cat = $_POST['product_cat'];
+                        // $product_brand = $_POST['product_brand'];
+                        // $product_price = $_POST['product_price'];
+                        // $product_desc = $_POST['product_desc'];
+                        // //getting images
+                        // $product_image = $_FILES['product_image']['name'];
+                        // $product_image_tmp = $_FILES['product_image']['tmp_name'];
+                        // move_uploaded_file($product_image_tmp, "product_images/$product_image");
+                        // //query to insert
+                        // $insert_product = "insert into products (product_cat, product_brand, product_title, product_price, product_desc, product_img) values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image')";
+                        // $insert_pro = mysqli_query($con, $insert_product);
+                        // if($insert_pro)
+                        // {
+                        // echo "<script>alert('Product inserted')</script>";
+                        // echo "<script>window.open('products.php','_self')</script>";
+                        // }
+                        // }
                     }
                     else if ($link == '4')
                     {
