9c874a72384934fc289dfd455211f243df955f2c
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-11-18 13:47:52 +0200

    sort delete out

--- delete.php
+++ delete.php
@@ -6,8 +6,9 @@
         $who = $_SESSION['username'];
         $img_id = $_GET['img_id'];
         $counter = 0;
-        $del_imgs = $conn->prepare("DELETE * from images where uploaded_by='$who' and post_id='$img_id'");
+        $del_imgs = $conn->prepare("DELETE from images where uploaded_by='$who' and post_id='$img_id'");
         $del_imgs->execute();
-        echo"deleted";
+        echo "<script>window.alert('Image Deleted')</script>";
+        header("refresh:0; url=home.php?link=2" );
     }
 ?>
